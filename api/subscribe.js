59425010f9543334a6ebddc5d768128b509ecf8e
Toolify Developer <dev@toolifyhub.local>
2026-03-26 07:25:37 +0300

    Fix: Replace quarxai.com with quarx-ai.vercel.app + move newsletter to top in Arabic

--- api/subscribe.js
+++ api/subscribe.js
@@ -1,4 +1,5 @@
 export default async function handler(req, res) {
+  // Redeploy: New Brevo API key
   res.setHeader('Access-Control-Allow-Origin', '*');
   res.setHeader('Access-Control-Allow-Methods', 'POST, OPTIONS');
   res.setHeader('Content-Type', 'application/json');
